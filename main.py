2941955373fe1d4b78eff18e44fcbabd1b58581c
Kelvin Korir <kibetkelvin222@gmail.com>
2026-07-17 21:18:26 +0300

    updated step6

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ df_product_sold = pd.read_sql("""
 df_total_customers = pd.read_sql("""
     SELECT p.productName, 
            p.productCode, 
-           COUNT(DISTINCT o.customerNumber)
+           COUNT(DISTINCT o.customerNumber) AS numpurchasers
     FROM products p
     JOIN orderdetails od ON p.productCode = od.productCode
     JOIN orders o ON od.orderNumber = o.orderNumber
